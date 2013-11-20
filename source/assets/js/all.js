0c7021e5690df526e1583e1e93226c3dc2fd3a3d
Nikolas Wise <nikolaswise@gmail.com>
2013-11-20 12:05:31 -0800

    init and display tied to firebase

--- source/assets/js/all.js
+++ source/assets/js/all.js
@@ -1,12 +1,12 @@
 // INIT
 var Pocket = new Firebase('https://pocket.firebaseio.com/');
-var display = document.getElementById('pocket-value');
+var count = document.getElementById('pocket-value');
 
 var p = {};
 
 Pocket.on('value', function(snapshot) {
   p = snapshot.val();
-  display.innerHTML = p.current;
+  count.innerHTML = p.current;
 });
 
 // SET
