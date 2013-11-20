fd3161762e8d296ce58c143424f4de1b847ac8ac
Nikolas Wise <nikolaswise@gmail.com>
2013-11-20 13:23:16 -0800

    add hammer touch events

--- source/assets/js/all.js
+++ source/assets/js/all.js
@@ -3,6 +3,12 @@ var Pocket = new Firebase('https://pocket.firebaseio.com/');
 var count = document.getElementById('pocket-value');
 
 var p = {};
+var undid = false;
+
+var current = Pocket.child('current'),
+    last    = Pocket.child('last'),
+    start   = Pocket.child('start');
+
 
 Pocket.on('value', function(snapshot) {
   p = snapshot.val();
@@ -10,9 +16,37 @@ Pocket.on('value', function(snapshot) {
 });
 
 // SET
+var set = function(val) {
+  start.set(val);
+  current.set(val);
+  last.set('');
+  undid = false;
+};
 
 // SUBTRACT
+var subtract = function(val) {
+  var latest = p.current - val;
+  current.set(latest);
+  last.set(val);
+  undid = false;
+};
 
-// RESET
+// UNDO/REDO
+var undo = function() {
+  val = p.current+p.last;
+  current.set(val);
+  undid = true;
+};
+
+var redo = function() {
+  if (undid === true) {
+    val = p.current-p.last;
+    current.set(val);
+    undid = false;
+  } else {
+    console.log('Nothing to Undo.');
+  }
+};
 
 // ERROR
+
