dbce56c4a9eeebad76f5aee02399243785fb032c
Nikolas Wise <nikolaswise@gmail.com>
2013-11-21 12:12:55 -0800

    reorder js

--- source/assets/js/firebase.js
+++ source/assets/js/firebase.js
@@ -1,22 +1,25 @@
 // INIT
-var Pocket = new Firebase('https://pocket.firebaseio.com/');
+
+var Pocket = Pocket || {};
+
+var ref = new Firebase('https://pocket.firebaseio.com/');
 var count = document.getElementById('pocket-value');
 
 var p = {};
 var undid = false;
 
-var current = Pocket.child('current'),
-    last    = Pocket.child('last'),
-    start   = Pocket.child('start');
+var current = ref.child('current'),
+    last    = ref.child('last'),
+    start   = ref.child('start');
 
 
-Pocket.on('value', function(snapshot) {
+ref.on('value', function(snapshot) {
   p = snapshot.val();
   count.innerHTML = p.current;
 });
 
 // SET
-var set = function(val) {
+Pocket.set = function(val) {
   start.set(val);
   current.set(val);
   last.set('');
@@ -24,7 +27,7 @@ var set = function(val) {
 };
 
 // SUBTRACT
-var subtract = function(val) {
+Pocket.subtract = function(val) {
   var latest = p.current - val;
   current.set(latest);
   last.set(val);
@@ -32,13 +35,13 @@ var subtract = function(val) {
 };
 
 // UNDO/REDO
-var undo = function() {
+Pocket.undo = function() {
   val = p.current+p.last;
   current.set(val);
   undid = true;
 };
 
-var redo = function() {
+Pocket.redo = function() {
   if (undid === true) {
     val = p.current-p.last;
     current.set(val);
---
+++ source/assets/js/keyboard.js
@@ -0,0 +1,19 @@
+// ERROR
+
+// KEYBOARD
+// make a keyboard that talks to an output zone.
+  // make a string stuff the output
+  //for each number on keyboard
+    // Find it's value
+    // on keypress,
+      // add value to string
+  // on delete key press
+    // remove last character from string
+  // on enter key press
+    // remove keyboard
+    // return string
+
+// Edit Set
+// on set click
+  // call keyboard
+  // on enter
