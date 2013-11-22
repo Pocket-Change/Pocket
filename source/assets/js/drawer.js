108a4bc7e1412e57df20fe5d3747711e36fa42ce
Nikolas Wise <nikolaswise@gmail.com>
2013-11-22 13:24:55 -0800

    hammer working better, undo and redo wired, MVP attained

--- source/assets/js/drawer.js
+++ source/assets/js/drawer.js
@@ -7,21 +7,43 @@ Framework agnostic, depends on Modernizr & Classie.
 var body        = document.getElementById('wrap'),
     drawer      = document.getElementById('drawer'),
     // buttons     = Array.prototype.slice.call(document.querySelectorAll('.toggle')),
-    // touchEvent  = 'click',
+    touchEvent  = 'click',
     content     = document.getElementById('main-content');
+
+// Add/Remove class helpers.
+var hasClass = function (elem, className) {
+  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+};
+
+var addClass = function (elem, className) {
+  if (!hasClass(elem, className)) {
+    elem.className += ' ' + className;
+  }
+};
+
+var removeClass = function (elem, className) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+      newClass = newClass.replace(' ' + className + ' ', ' ');
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  }
+};
+
 
 var Drawer = {
   closeDrawer: function(){
-    classie.remove(drawer, 'active-left');
-    classie.remove(drawer, 'active-right');
-    classie.remove(drawer, 'active-top');
-    classie.remove(drawer, 'active-bottom');
-    classie.remove(body, 'drawer-open');
+    removeClass(drawer, 'active-left');
+    removeClass(drawer, 'active-right');
+    removeClass(drawer, 'active-top');
+    removeClass(drawer, 'active-bottom');
+    removeClass(body, 'drawer-open');
   },
 
   openDrawer: function(direction){
-    classie.add(drawer, direction);
-    classie.add(body, 'drawer-open');
+    addClass(drawer, direction);
+    addClass(body, 'drawer-open');
   },
 
   bodyClick: function(e){
